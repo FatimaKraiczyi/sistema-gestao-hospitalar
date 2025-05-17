54ae63370ec50a392409272d423001c4ec1f52ec
Fátima Kraiczyi <fatimakraiczyi@gmail.com>
2025-05-17 13:49:30 -0300

    feat: initialize consulta-service and paciente-service microservices
    - Updated docker-compose.yml to include paciente-service and consulta-service with their respective database services.
    - Created Dockerfile and Maven wrapper files for both services.

---
+++ paciente-service/src/main/java/br/com/gestao_hospitalar/paciente_service/PacienteServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.gestao_hospitalar.paciente_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacienteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacienteServiceApplication.class, args);
+	}
+
+}
